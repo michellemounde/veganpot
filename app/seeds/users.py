a594bc953338174cf82aafbb19ea42583792da6e
Michelle Mounde <mitchmounde@gmail.com>
2023-05-10 13:35:50 +0300

    build: undo seeds in production before seeding for deployment

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,12 +4,19 @@ from app.models import db, User, debug, SCHEMA
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
+    if debug == "False":
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
+        # the schema name (see comment in users.py undo_users function).
+        # Make sure to add all your other model's undo functions below
+        undo_users()
+
     demo = User(
         username='Demo-lition', first_name='Demoli', last_name='Tion', email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', first_name='Marnie', last_name='User', email='marnie@aa.io', password='password')
+        username='marnie', first_name='Marnie', last_name='User', email='marnie@aa.io', password='password1')
     bobbie = User(
-        username='bobbie', first_name='Bobbie', last_name='User', email='bobbie@aa.io', password='password')
+        username='bobbie', first_name='Bobbie', last_name='User', email='bobbie@aa.io', password='password2')
 
     db.session.add(demo)
     db.session.add(marnie)
